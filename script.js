a3c550f35d660a72b1926128b49415118692dfba
Arzuuuu <arzu22786@gmail.com>
2023-07-20 16:01:45 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('show');
+    document.querySelector('.menu-toggle').classList.toggle('change');
+  });
+  
